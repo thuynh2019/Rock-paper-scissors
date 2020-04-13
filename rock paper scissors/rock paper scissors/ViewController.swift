69c7f01d4583963eca483209c48f37b5fe2f4f15
thuynh2019 <ty.huynh@smail.astate.edu>
2020-04-13 01:28:04 -0500

    commit

--- rock paper scissors/rock paper scissors/ViewController.swift
+++ rock paper scissors/rock paper scissors/ViewController.swift
@@ -14,21 +14,91 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBOutlet weak var winLabel: UILabel!
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var cpuLabel: UILabel!
     
+    var player: Int = 0
+    var rival: Int = 0
+    
     @IBAction func rock(_ sender: Any) {
+        var player = 1
         myLabel.text = "rock"
+        rival = Int.random(in: 1...3)
+        if rival != 0 {
+            switch rival {
+            case 1:
+                cpuLabel.text = "rock"
+            case 2:
+            cpuLabel.text = "paper"
+            case 3:
+            cpuLabel.text = "scissors"
+            default:
+                break
+            }
+        }
+        winCheck()
     }
     @IBAction func paper(_ sender: Any) {
+        var player = 2
         myLabel.text = "paper"
+        rival = Int.random(in: 1...3)
+        if rival != 0 {
+            switch rival {
+            case 1:
+                cpuLabel.text = "rock"
+            case 2:
+            cpuLabel.text = "paper"
+            case 3:
+            cpuLabel.text = "scissors"
+            default:
+                break
+            }
+        }
+        winCheck()
     }
     @IBAction func scissor(_ sender: Any) {
+        var player = 3
         myLabel.text = "scissors"
+        rival = Int.random(in: 1...3)
+        if rival != 0 {
+            switch rival {
+            case 1:
+                cpuLabel.text = "rock"
+            case 2:
+            cpuLabel.text = "paper"
+            case 3:
+            cpuLabel.text = "scissors"
+            default:
+                break
+            }
+        }
+        winCheck()
     }
     
     func winCheck(){
+        if player == rival {
+            winLabel.text = "Tie!"
+        }
+        if player == 1 && rival == 2 {
+            winLabel.text = "Lose!"
+        }
+        if player == 1 && rival == 3 {
+            winLabel.text = "Win!"
+        }
+        if player == 2 && rival == 1 {
+            winLabel.text = "Win!"
+        }
+        if player == 2 && rival == 3 {
+            winLabel.text = "Lose!"
+        }
+        if player == 3 && rival == 1 {
+            winLabel.text = "Lose!"
+        }
+        if player == 3 && rival == 2 {
+            winLabel.text = "Win!"
+        }
         
     }
     
